6afc5eb2343e6b755e2b26501d7f5b23bdfa9513
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2021-04-02 13:16:53 +0200

    apolloProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <h2>No to Zaczynamy zabawe z Reactem :)</h2>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <h2>No to Zaczynamy zabawe z Reactem :)</h2>
+      </div>
+    </ApolloProvider>
   );
 }
 
